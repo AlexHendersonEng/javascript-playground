06ec4cfff4e6407beb2796f8df3d5166026d4f32
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-12 13:20:31 +0000

    Added strings project

---
+++ strings/main.js
@@ -0,0 +1,15 @@
+// Strings can be defined with single or double quotes
+let a = 'Hello World!';
+let b = "Goodbye World!";
+
+/* Template strings allow for multiline strings and allow for variables
+   to be inserted into a string */
+let c = 'World!'
+let d = `This is a
+multiline string.
+Hello ${c}.`
+
+// Print strings to console
+console.log(a);
+console.log(b);
+console.log(d);
