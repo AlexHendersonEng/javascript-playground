65b43b6f6c90d5dfd6de4042c7f11319dc8a19da
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-12 15:57:14 +0000

    Added strict_equality project

---
+++ strict_equality/main.js
@@ -0,0 +1,15 @@
+/* 
+    Equality operator (==): Compare if values are equal.
+    Strict equality operator (===): Compare if values and datatype are equal.
+    Inequality operator (!=): Compare if values are not equal.
+    Strict inequality operator (!==): Compare if values or datatype are not equal.
+*/
+
+// Define variable for comparisons
+const PI = 3.14;
+
+// Use equality operator
+(PI == "3.14") ? console.log("Equal") : console.log("Not equal");
+
+// Use strict equality operator
+(PI === "3.14") ? console.log("Equal") : console.log("Not equal");
