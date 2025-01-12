f985a68c27995b5ac09c427c05203dc82ed53171
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-12 12:33:15 +0000

    Added basic_website project

---
+++ basic_website/index.js
@@ -0,0 +1,15 @@
+// A single line comment
+
+/*
+    A multiline comment
+*/
+
+// Log some text to console
+console.log(`Hello World!`)
+
+// Create an alert
+window.alert(`Alert`)
+
+// Get and modify Document Object Model (DOM) element
+document.getElementById('my-h1').textContent = `My Heading`;
+document.getElementById('my-p').textContent = `My Paragraph`;
